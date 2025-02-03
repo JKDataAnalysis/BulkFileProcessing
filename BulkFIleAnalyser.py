d77990e44d044f8ea74565b89f2bbbbfbd5e9958
J Kelly <jon.kelly@tuta.io>
2025-02-03 18:31:27 +0000

    Change: redesign of window layout Change: Files are now added to the cue rather than selecting a folder. This allows adding files from different folders Fix: the number of files cued now updates Fix: The reset button works again New: Button added to vue/ edit the file cue (currently only allows viewing)

--- BulkFIleAnalyser.py
+++ BulkFIleAnalyser.py
@@ -1,13 +1,11 @@
-"""
+'''
 TODO
-    * When file type is selected, update the window with selected type
     + Look at binding multiple functions so that updating the window does not need to be within the class definition
+    * Resolve remaining problems including instance attribution outside of __init__
     * Modify get path function as a class to return list of files of set type within the chosen folder
     (See Test01.py)
-        - Update window to show path
-        - Update window to show number of files of type
+    * (Add button to show and edit file list)
     * Add validation to read of data file definitions
-    * Create subclass of tkinter button class to return values of selected data folder and number of files cued
     --- Start of loop
         * Read in first file
         * Process data in first file
@@ -16,24 +14,16 @@ TODO
     * Write results df to file
     Known bugs
     ==========
-"""
-import glob
+'''
 import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import messagebox
 from tkinter import ttk
-from tkinter.filedialog import askdirectory
-# import pandas as pd
 import numpy as np
-import csv
 import os
 import sys
-import fnmatch
-# from tkinter import ttk
 from tkinter import *
 
-def temp_show_info(passed_info):
-    print(f"Passed info: >>>{passed_info.import_settings}<<<")
 
 def quit_script():
     print("Leaving program")
@@ -62,87 +52,115 @@ def get_file_definitions():
         )
         return -1
 
-class Get_filetype:
+
+class GetFiles:
     def __init__(self, defined_filetypes):
-        self.file_defs = defined_filetypes # Copy passed filetypes so they will be available for other class functions
-        self.type_list = self.file_defs['source'].tolist()   # Strip out list of defined source types
+        self.file_defs = defined_filetypes  # Copy passed filetypes so they will be available for other class functions
+        self.type_list = defined_filetypes['source'].tolist()   # Strip out list of defined source types
         # create choose data file type label
-        choose_data_filetype_frm = LabelFrame(main_window, text="Select data file type")
-        choose_data_filetype_frm.pack(padx=pdx, pady=pdy, side=tk.LEFT )
+        choose_data_filetype_lblfrm = LabelFrame(main_window, text="Select data file type")
+        choose_data_filetype_lblfrm.pack(padx=pdx, pady=pdy, side=tk.TOP, anchor=tk.NW)
+
         # create combobox object to select data file type
-        self.defined_types = tk.StringVar()
-        data_filetype_combo = ttk.Combobox(choose_data_filetype_frm, textvariable=self.defined_types, state='readonly')
-        data_filetype_combo['values'] = self.type_list
-        data_filetype_combo.bind('<<ComboboxSelected>>', self.get_import_file_defs)  # bind function for when value changed
-        data_filetype_combo.pack(padx=pdx, pady=pdy)
+        defined_types = tk.StringVar()
+        # create combobox with self. so it can be used in the reset function
+        self.data_filetype_combo = ttk.Combobox(choose_data_filetype_lblfrm, textvariable=defined_filetypes, state='readonly')
+        self.data_filetype_combo['values'] = self.type_list
+        self.data_filetype_combo.bind('<<ComboboxSelected>>', self.get_import_file_defs)  # bind function for when value changed
+        self.data_filetype_combo.pack(padx=pdx, pady=pdy)
+
+        # Create a frame for file cue widgets
+        data_files_lblfrm = LabelFrame(main_window, text="File cue")
+        data_files_lblfrm.pack(padx=pdx, pady=pdy, side=tk.TOP, anchor=tk.NW)
+        # create button to add files
+        self.add_files_btn = ttk.Button(
+            data_files_lblfrm,
+            text="Add files",
+            command=lambda: self.add_files(self.import_settings['file_type'][0]),
+            state="disabled"
+        )
+        self.add_files_btn.pack(side=tk.TOP, padx=pdx, pady=pdy)
+        # create button to view/ edit file cue
+        self.edit_files_btn = ttk.Button(
+            data_files_lblfrm,
+            text="View/ edit cue",
+            command=lambda: self.edit_cue(self.cued_file_list),
+            state="disabled"
+        )
+        self.edit_files_btn.pack(side=tk.TOP, padx=pdx, pady=pdy)
+
+        # Create label of files in cue
+        self.cued_file_count = 0
+        self.cued_file_count_lbl = Label(data_files_lblfrm)
+        self.update_cue()
+        self.cued_file_count_lbl.pack(side=tk.TOP, padx=pdx, pady=pdy)
 
     def get_import_file_defs(self, event):
         self.import_settings = self.file_defs[self.file_defs['source'] == event.widget.get()]
+        event.widget['state'] = 'disabled'  # Disable combox box so that filetype can't be changed after folder is selected
+        self.add_files_btn['state'] = 'normal'   # Enable the data folder button
 
-
-def data_filetype_selected(event):
-    file_type_label_gridinfo = choose_data_filetype_lbl.grid_info()   # Get grid info of existing label
-    # Remove selection combo and label
-    choose_data_filetype_lbl.grid_remove()
-    data_filetype_combo.grid_remove()
-    # Grid the label frame
-    # data_filetype_frm.grid(
-        # row=file_type_label_gridinfo['row'],
-        # column=file_type_label_gridinfo['column'],
-    #     padx=pdx, pady=pdy
-    # )
-    # Add details of what selected
-    data_filetype_lbl['text'] = event.widget.get()
-    data_filetype_lbl.grid()
-    choose_data_fldr_btn['state'] = tk.NORMAL
-
-
-def get_data_folder(filetype):
-    path = askdirectory(title='Select data Folder')  # shows dialog box and return the path
-    if (len(path)) > 0:  # Cancel not clicked
-        btn_grid_info = choose_data_fldr_btn.grid_info()  # Get grid location of choose data folder button
-        choose_data_fldr_btn.grid_remove()   # Remove choose data folder button
-        # replace button with label
-        data_fldr_frm.grid(
-            row=btn_grid_info['row'],
-            column=btn_grid_info['column'],
-            padx=pdx, pady=pdy,
-            columnspan=4
+    def add_files(self, file_type):
+        filetypes = (
+            ('Files of type in definitions', "*." + file_type),
+            ('All files (may not be compatible)', '*.*')
         )
-        # Add label for path
-        data_fldr_path_lbl['text'] = path
-        data_fldr_path_lbl.grid(row=0, column=0, padx=pdx, pady=pdy, columnspan=4)
-        # count number of files in folder with the correct extension
-        f_type = filetype["file_type"][0]
-        filelist = glob.glob(os.path.join(path, "*." + f_type))
-        f_count = len(filelist)
-        # make label visible and add text
-        file_cue_lbl['text'] = f"{f_count} files with extension '{f_type}' found"
-        file_cue_lbl.grid(row=1, column=0, padx=pdx, pady=pdy, columnspan=4)
-        if f_count > 0:  # If some files were found, enable analysis
-            run_analysis_btn['state'] = tk.NORMAL
+        selected_file_list = fd.askopenfilenames(
+            title='Open files',
+            # initialdir='/',
+            filetypes=filetypes
+        )
+        try:  # If a cue has already been created, add the files to it
+            self.cued_file_list += selected_file_list
+        except AttributeError:  # If no existing cue, create a tuple for it
+            self.cued_file_list = selected_file_list
+
+        # The difference between the tuple length (allow duplicates) and the set length (no duplicates) will be the
+        # number of duplicates
+        duplicate_count = len(self.cued_file_list) - len(set(self.cued_file_list))
+        if duplicate_count > 0:
+            messagebox.showinfo(
+                title="Duplicates found",
+                message=f"{str(duplicate_count)} file(s) found that are already cued\nThese will not be added"
+            )
+            # Convert to a set then back to a tuple to remove duplicates
+            self.cued_file_list = tuple(set(self.cued_file_list))
+        self.cued_file_count = len(self.cued_file_list)
+        if self.cued_file_count > 0:  # If there are files in the cue, allow analysis to be run
+            run_analysis_btn['state'] = 'normal'
+            self.edit_files_btn['state']= 'normal'
+        self.update_cue()
+
+    def update_cue(self):
+        self.cued_file_count_lbl.config(text=str(self.cued_file_count) + " files cued")
+
+    def edit_cue(self, file_list):
+        file_cue_window = tk.Toplevel()
+        file_cue_window.title('File cue')
+        cue_list = tk.Variable(value=file_list)
+        file_list_list = tk.Listbox(file_cue_window, listvariable=cue_list, selectmode=MULTIPLE, width=0)
+        file_list_list.pack(expand=True, fill=tk.BOTH, padx=pdx, pady=pdy)
+        # Create a button to close (destroy) this window.
+        button_close = ttk.Button(
+            file_cue_window,
+            text="Close window",
+            command=file_cue_window.destroy
+        )
+        button_close.pack(padx=pdx, pady=pdy)
 
 
 def run_analysis():
     messagebox.showinfo(title="TO ADD", message="Run analysis not yet implemented")
 
 
-def reset_window():
-    # reset choose data file type
-    choose_data_filetype_lbl.grid()
-    data_filetype_combo.grid()
-    data_filetype_frm.grid_remove()
-    data_filetype_lbl.grid_remove()
-    data_filetype_combo.set("")
-
-    # reset data folder
-    data_fldr_frm.grid_remove()
-    data_fldr_path_lbl.grid_remove()
-    choose_data_fldr_btn.grid()
-    choose_data_fldr_btn['state'] = tk.DISABLED
-    file_cue_lbl.grid_remove()
-    # reset run analysis button
-    run_analysis_btn['state'] = tk.DISABLED
+def reset_window(filetype_combo, add_files_btn, edit_files_btn):
+    filetype_combo['state'] = 'normal'  # Reactivate file type combo box
+    add_files_btn['state'] = tk.DISABLED  # Disable add files button
+    edit_files_btn['state'] = tk.DISABLED  # Disable view/ edit file cue button
+    run_analysis_btn['state'] = tk.DISABLED  # Disable run analysis button
+    file_import_settings.cued_file_list = tuple()   # Clear the file cue
+    file_import_settings.cued_file_count = 0    # zero the file cue length
+    file_import_settings.update_cue()   # Update the displayed cue length
 
 
 # === START OF MAINLOOP ===
@@ -156,61 +174,42 @@ pdy = 5
 # Get data file definitions
 data_file_definitions = get_file_definitions()  # Read in data file specifications
 if type(data_file_definitions) is int:  # If script has returned an error code: exit
-    quitScript()
-file_import_settings = Get_filetype(data_file_definitions)
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-temp_btn = Button(main_window, text="Show", command=lambda :temp_show_info(file_import_settings))
-temp_btn.pack()
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Get_filetype.
-# choose_data_filetype_lbl.grid()
-# create choose data file type label
-choose_data_filetype_lbl = Label(main_window, text="Select data file type")
-# choose_data_filetype_lbl.grid(row=0, column=0, padx=pdx, pady=pdy, sticky=tk.W)
-# create chosen data file type labels but don't place in grid yet
-data_filetype_frm = LabelFrame(main_window, text='Data file type')
-data_filetype_lbl = Label(data_filetype_frm)
+    quit_script()
+file_import_settings = GetFiles(data_file_definitions)
 
-# create combobox object to select data file type
-filetype_list = tk.StringVar()
-data_filetype_combo = ttk.Combobox(main_window, textvariable=filetype_list, state='readonly')
-data_filetype_combo['values'] = data_file_definitions['source'].tolist()  # convert to list to remove [] and ''
-# data_filetype_combo['state'] = 'readonly'  # Prevent adding custom combobox values
-# data_filetype_combo.bind('<<ComboboxSelected>>', data_filetype_selected)  # bind function for when value changed
-# data_filetype_combo.grid(row=0, column=1, padx=pdx, pady=pdy, sticky=tk.W)
-
-# create choose folder containing data button
-
-choose_data_fldr_btn = ttk.Button(
-    main_window,
-    text="Select data folder",
-    command=lambda: get_data_folder(data_file_definitions[data_file_definitions['source'] == filetype_list.get()]),
-    state="disabled"
-)
-# choose_data_fldr_btn.grid(row=2, column=0, padx=pdx, pady=pdy, sticky=tk.W)
-
-# create labels for data folder chosen and number of files cued but don't display yet
-data_fldr_frm = LabelFrame(main_window, text="Data folder")
-data_fldr_path_lbl = Label(data_fldr_frm)
-file_cue_lbl = Label(data_fldr_frm)
+btn_frame = tk.Frame(main_window, borderwidth=5, relief=tk.RAISED, bg='red', width=500)
+btn_frame.pack(side=tk.BOTTOM)
 
 # Create run analysis button
 run_analysis_btn = ttk.Button(
-    main_window,
+    btn_frame,
     text="Run analysis",
     state="disabled",
     command=run_analysis
 )
+run_analysis_btn.pack(padx=pdx, pady=pdy, side=tk.LEFT)
 
 # Create reset button
-reset_btn = ttk.Button(main_window, text="Reset", command=reset_window)
+reset_btn = ttk.Button(
+    btn_frame,
+    text="Reset",
+    command=lambda: reset_window(
+        file_import_settings.data_filetype_combo,
+        file_import_settings.add_files_btn,
+        file_import_settings.edit_files_btn
+    )
+)
+reset_btn.pack(padx=pdx, pady=pdy, side=tk.LEFT)
 # reset_btn.grid(row=7, column=0, padx=pdx, pady=pdy)
 
 # Create quit script button
-quit_button = ttk.Button(main_window, text="Quit", command=quit_script)
+quit_btn = ttk.Button(btn_frame, text="Quit", command=quit_script)
+quit_btn.pack(padx=pdx, pady=pdy, side=tk.LEFT)
 # quit_button.grid(row=7, column=1, padx=pdx, pady=pdy)
 
+run_analysis_btn.pack(padx=pdx, pady=pdy, side=tk.RIGHT)
 # run_analysis_btn.grid(row=7, column=3, padx=pdx, pady=pdy)
-
 # Display until User exits themselves.
+
+
 main_window.mainloop()
